22869a76a5d867d63478c2910511a5bfd4ea28cc
Fredrik Larsson <nossralf@gmail.com>
2019-10-19 22:52:00 +0200

    Make constants class and members package private

--- src/main/java/com/analogmountains/flume/MongoSinkConstants.java
+++ src/main/java/com/analogmountains/flume/MongoSinkConstants.java
@@ -7,42 +7,42 @@
  */
 package com.analogmountains.flume;
 
-public class MongoSinkConstants {
+class MongoSinkConstants {
 
   /**
    * Comma-separated list of hostname:port. If the port is not present the
    * default port 27017 will be used.
    */
-  public static final String HOSTNAMES = "hostNames";
+  static final String HOSTNAMES = "hostNames";
 
   /**
    * Database name.
    */
-  public static final String DATABASE = "database";
+  static final String DATABASE = "database";
 
   /**
    * Collection name.
    */
-  public static final String COLLECTION = "collection";
+  static final String COLLECTION = "collection";
 
   /**
    * User name.
    */
-  public static final String USER = "user";
+  static final String USER = "user";
 
   /**
    * Password.
    */
-  public static final String PASSWORD = "password";
+  static final String PASSWORD = "password";
 
   /**
    * Maximum number of events the sink should take from the channel per
    * transaction, if available. Defaults to 100.
    */
-  public static final String BATCH_SIZE = "batchSize";
+  static final String BATCH_SIZE = "batchSize";
 
   /**
    * The default batch size.
    */
-  public static final int DEFAULT_BATCH_SIZE = 100;
+  static final int DEFAULT_BATCH_SIZE = 100;
 }
