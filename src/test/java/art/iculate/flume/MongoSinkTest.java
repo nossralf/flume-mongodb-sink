47bdb9f053b795da05d9cd816c548da8952d6a1a
Fredrik Larsson <nossralf@gmail.com>
2022-09-20 23:46:01 +0200

    Parameterize test to remove SonarQube code smell

--- src/test/java/art/iculate/flume/MongoSinkTest.java
+++ src/test/java/art/iculate/flume/MongoSinkTest.java
@@ -2,41 +2,47 @@ package art.iculate.flume;
 
 import com.mongodb.ServerAddress;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(Parameterized.class)
 public class MongoSinkTest {
 
-  @Test
-  public void serverAddressesWithoutPortsGetDefaultMongoPort() {
-    List<ServerAddress> expected = new ArrayList<>();
-    expected.add(new ServerAddress("host1", 27017));
-    expected.add(new ServerAddress("host2", 27017));
-
-    assertThat(MongoSink.parseHostnames("host1,host2"), is(equalTo(expected)));
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        {"host1,host2", 27017, 27017},
+        {"host1:1234,host2:2345", 1234, 2345},
+        {"  host1:1234, host2:2345  ", 1234, 2345},
+      });
   }
 
+  @Parameter(0)
+  public String hostNames;
+
+  @Parameter(1)
+  public int firstPort;
+
+  @Parameter(2)
+  public int secondPort;
+
   @Test
-  public void serverAddressesWithPorts() {
+  public void serverAddresses() {
     List<ServerAddress> expected = new ArrayList<>();
-    expected.add(new ServerAddress("host1", 1234));
-    expected.add(new ServerAddress("host2", 2345));
+    expected.add(new ServerAddress("host1", firstPort));
+    expected.add(new ServerAddress("host2", secondPort));
 
-    assertThat(MongoSink.parseHostnames("host1:1234,host2:2345"), is(equalTo(expected)));
+    assertThat(MongoSink.parseHostnames(hostNames), is(equalTo(expected)));
   }
-
-  @Test
-  public void junkWhitespaceIsRemovedFromHostNames() {
-    List<ServerAddress> expected = new ArrayList<>();
-    expected.add(new ServerAddress("host1", 1234));
-    expected.add(new ServerAddress("host2", 2345));
-
-    assertThat(MongoSink.parseHostnames("  host1:1234, host2:2345  "), is(equalTo(expected)));
-  }
-
 }
