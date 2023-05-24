5fbb6b172986b2c49e5097ddc3765c60c435849a
Fredrik Larsson <nossralf@gmail.com>
2023-05-24 22:01:39 +0200

    Upgrade to Gradle 8.1, switch to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    jacoco
+    `java-library`
+    id("org.sonarqube") version "4.0.0.2929"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val flumeVersion = "1.11.0"
+
+dependencies {
+    api("org.apache.flume:flume-ng-configuration:${flumeVersion}")
+    api("org.apache.flume:flume-ng-core:${flumeVersion}")
+    api("org.apache.flume:flume-ng-sdk:${flumeVersion}")
+
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.mongodb:mongodb-driver:3.12.12")
+    implementation("org.slf4j:slf4j-api:2.0.4")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
+}
+
+group = "art.iculate.flume"
+version = "0.2"
+description = "flume-mongodb-sink"
+
+java {
+     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.setDeprecation(true)
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "nossralf_flume-mongodb-sink")
+        property("sonar.organization", "nossralf")
+        property("sonar.host.url", "https://sonarcloud.io")
+  }
+}
+
+tasks.check {
+    finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.sonar)
+}
